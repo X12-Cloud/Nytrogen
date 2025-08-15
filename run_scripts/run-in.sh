f6ff337cb2ae89ad5bef075f58ebe474a1731846
X12-Cloud <lamadasun888@gmail.com>
2025-08-15 22:28:42 +0300

    added test files in the tests directory and enhanced the run commands to support agruments (Other fixes still coming)

---
+++ run_scripts/run-in.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+echo "Enter the file name without the estension"
+read input
+./build/Nytro "$input.nyt"
+
+echo ""
+echo "--- Assembling out.asm ---"
+nasm -f elf64 out/out.asm -o out/out.o
+
+echo ""
+echo "--- Linking ---"
+ld -o out/out out/out.o -lc --dynamic-linker /usr/lib64/ld-linux-x86-64.so.2
+
+echo ""
+echo "--- Running output program ---"
+./out/out
+echo "Exit Code: $?"
+
+
