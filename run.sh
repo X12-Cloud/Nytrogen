1b90af660766898a2ac5570d11273887e0f7aaaa
X12-Cloud <lamadasun888@gmail.com>
2026-02-13 11:46:47 +0200

    feat: made run.sh use the wrapper

--- run.sh
+++ run.sh
@@ -4,19 +4,15 @@
 SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
 
 # Default values
-disable_preprocessor=false
 clean_build=false
 build=false
-object_file_only=false
 input_file=""
 
 # Parse command-line arguments
 while [[ "$#" -gt 0 ]]; do
     case $1 in
-        -dp|--disable-preprocessor) disable_preprocessor=true ;;
         -clean|--clean-build) clean_build=true ;;
         -build|--build) build=true ;;
-        -obj|--object-file) object_file_only=true ;;
         *) input_file="$1" ;;
     esac
     shift
@@ -27,6 +23,7 @@ if [ "$clean_build" = true ]; then
     echo "--- Performing a clean build of Nytrogen and Preprocessor ---"
     "$SCRIPT_DIR/run_scripts/clean_build.sh"
     (cd "$SCRIPT_DIR/Preprocessor" && ./clean_build.sh)
+    (cd "$SCRIPT_DIR/driver" && ./build.sh)
     echo "--- Clean build complete ---"
     exit 0
 fi
@@ -36,50 +33,16 @@ if [ "$build" = true ]; then
     echo "--- Performing an incremental build of Nytrogen and Preprocessor ---"
     "$SCRIPT_DIR/run_scripts/build.sh"
     (cd "$SCRIPT_DIR/Preprocessor" && ./build.sh)
+    (cd "$SCRIPT_DIR/driver" && ./build.sh)
     echo "--- Incremental build complete ---"
     exit 0
 fi
 
 # Check if input file is provided
 if [ -z "$input_file" ]; then
-    echo "Usage: $0 [-dp] [-clean] [-build] <input_file.nyt>"
+    echo "Usage: $0 [-clean] [-build] <input_file.nyt>"
     exit 1
 fi
 
-# Create out directory in the current working directory
-CWD=$(pwd)
-OUT_DIR="$CWD/out"
-mkdir -p "$OUT_DIR"
-
-# Run preprocessor if not disabled
-if [ "$disable_preprocessor" = false ]; then
-    echo "--- Running Nytrogen Preprocessor ---"
-    preprocessed_file="$OUT_DIR/$(basename "${input_file%.nyt}.pre.nyt")"
-    "$SCRIPT_DIR/Preprocessor/run.sh" "$input_file" > "$preprocessed_file"
-    echo "--- Preprocessor finished ---"
-else
-    preprocessed_file="$input_file"
-fi
-
 # Run compiler
-echo "--- Running Nytrogen Compiler ---"
-"$SCRIPT_DIR/bootstrap/build/Nytro" "$preprocessed_file" "$OUT_DIR/out.asm"
-
-echo ""
-echo "--- Assembling out.asm ---"
-output_obj_file="$OUT_DIR/$(basename "${input_file%.nyt}.o")"
-nasm -f elf64 "$OUT_DIR/out.asm" -o "$output_obj_file"
-
-if [ "$object_file_only" = true ]; then
-    echo "--- Object file generated: $output_obj_file ---"
-    exit 0
-fi
-
-echo ""
-echo "--- Linking ---"
-ld -o "$OUT_DIR/out" "$output_obj_file" -lc --dynamic-linker /usr/lib64/ld-linux-x86-64.so.2
-
-echo ""
-echo "--- Running output program ---"
-"$OUT_DIR/out"
-echo "Exit Code: $?"
+"$SCRIPT_DIR/driver/build/nytro" "$input_file"
