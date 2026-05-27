ec64c5f951625a2bb0d5ef2630f8465f110db888
X12-Cloud <lamadasun888@gmail.com>
2026-05-27 17:12:14 +0300

    feat: initial register allocator structure

---
+++ compiler/include/register_allocator.hpp
@@ -0,0 +1,39 @@
+#ifndef REGISTER_ALLOCATOR_HPP
+#define REGISTER_ALLOCATOR_HPP
+
+#include <vector>
+#include <unordered_map>
+#include <string>
+
+enum RegID {
+    R10,
+    R11,
+    RBX,
+    RCX,
+    RDI,
+    RSI
+};
+
+struct RegStrings {
+    std::string byte1;
+    std::string byte4;
+    std::string byte8;
+};
+
+class RegisterAllocator {
+public:
+    RegisterAllocator();
+
+    RegID allocate();
+    void free_reg(RegID reg);
+
+    void reset();
+
+    std::string get_name(RegID reg, int byte_size);
+
+private:
+    std::vector<RegID> register_pool;
+    std::unordered_map<RegID, RegStrings> register_lookup;
+};
+
+#endif
---
+++ compiler/src/register_allocator.cpp
@@ -0,0 +1,47 @@
+#include "register_allocator.hpp"
+#include "utils.hpp"
+
+#include <algorithm>
+
+RegisterAllocator::RegisterAllocator() {
+    register_pool = { R10, R11, RBX, RCX, RDI, RSI };
+
+    register_lookup[R10] = { "r10b", "r10d", "r10" };
+    register_lookup[R11] = { "r11b", "r11d", "r11" };
+    register_lookup[RBX] = { "bl",   "ebx",  "rbx" };
+    register_lookup[RCX] = { "cl",   "ecx",  "rcx" };
+    register_lookup[RDI] = { "di",   "edi",  "rdi" };
+    register_lookup[RSI] = { "si",   "esi",  "rsi" };
+}
+
+std::string RegisterAllocator::get_name(RegID reg, int byte_size) {
+    const RegStrings& bundle = register_lookup[reg];
+
+    switch (byte_size) {
+        case 1: return bundle.byte1;
+        case 4: return bundle.byte4;
+        default: return bundle.byte8;
+    }
+}
+
+RegID RegisterAllocator::allocate() {
+    if (register_pool.empty()) { Utils::report_error("Register Allocator", "Register pool is empty."); }
+
+    RegID allocated_register = register_pool.back();
+    register_pool.pop_back();
+
+    return allocated_register;
+}
+
+void RegisterAllocator::free_reg(RegID reg) {
+    auto it = std::find(register_pool.begin(), register_pool.end(), reg);
+
+    if (it == register_pool.end()) {
+        register_pool.push_back(reg);
+    }
+}
+
+void RegisterAllocator::reset() {
+    register_pool = { R10, R11, RBX, RCX, RDI, RSI };
+    return;
+}
